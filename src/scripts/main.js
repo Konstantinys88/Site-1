04966bb2ec400154adc859592efe942f62ce026a
Konstantin0 <maschkovc@yandex.ru>
2023-03-10 15:17:01 +0300

    добавлена карусель

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
+// меню 
     var sidenav = document.querySelectorAll('.sidenav');
     var instances_sidenav = M.Sidenav.init(sidenav);
-
+// иальные окна
     var modal = document.querySelectorAll('.modal');
     var instances_modal = M.Modal.init(modal);
+// карусель
+    var gallery = document.querySelectorAll('.materialboxed');
+    var instances_gallery = M.Materialbox.init(gallery);
 });
 
 
