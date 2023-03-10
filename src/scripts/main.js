617841bcdcc64db7d443a694ba9890d382e63f3e
Konstantin0 <maschkovc@yandex.ru>
2023-03-10 13:29:36 +0300

    modals

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,16 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav);
+
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
 });
+
+
+
+
 
 
 
